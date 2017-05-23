d83346d6ddd2c59add423f740f0defcc5ad5888b
twohundredboys <jsonnen89@gmail.com>
2017-05-22 18:33:55 -0600

    Update GiiArchitect.py

--- GiiArchitect.py
+++ GiiArchitect.py
@@ -11,6 +11,7 @@ while True:
         print("START: Input filepath where new directories should be created.")
         filePath = str(input())
         os.chdir(filePath)
+        pathBank = filePath
         filePath = "clear"
         print("Current working directory: " + str(os.getcwd()) + "\n")
 
@@ -30,15 +31,20 @@ while True:
                 continue
                 
         #for each title in list titles, make a directory in cwd
+        #create a file "archi.txt" inside each new directory
         for title in titles:
             os.mkdir(title.rstrip('\n'))
+            newDir = pathBank + "/" + title.rstrip('\n')
+            os.chdir(newDir)
+            newText = open("archi.txt","w+")
+            #print(os.getcwd())
+            os.chdir(pathBank)
+            print(os.getcwd())
             print(title.rstrip('\n') + " created in " + os.getcwd()) 
             continue
                
     except (KeyboardInterrupt, SystemExit):
         continue
-    except (FileExistsError):
-        print("Error: Cannot create a directory that already exists.\n\nStarting over.\n\n")
+    except (FileNotFoundError, OSError, FileExistsError):
+        print ("Error: There was an issue.\nMake sure your filepath is correct and that you are not trying to create directories that already exist.\nStarting over.\n")
         continue
-            
-            
